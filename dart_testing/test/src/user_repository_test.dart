bd365f8dc79ef1bad4bb06becda0f34f5b841fe1
Yasha Gozwan Shuhada <yashagozwan13@gmail.com>
2024-12-10 01:42:17 +0700

    add desc in test

--- dart_testing/test/src/user_repository_test.dart
+++ dart_testing/test/src/user_repository_test.dart
@@ -14,12 +14,12 @@ void main() {
       repository = MockUserRepository();
     });
 
-    test('should return name when getName is called', () async {
+    test('should return name when method getName is called', () async {
       when(repository.getName()).thenAnswer((_) => Future.value('John Doe'));
       expect(await repository.getName(), 'John Doe');
     });
 
-    test('should return list of friend name when get friends is called',
+    test('should return list of friend name when method getFriends is called',
         () async {
       when(repository.getFriends()).thenAnswer(
         (realInvocation) => Future.delayed(
