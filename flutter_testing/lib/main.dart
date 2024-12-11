4b9db83293cead9a50b93e1df69c7076356824e4
yashagozwan <yashagozwan.shuhada@homecredit.co.id>
2024-12-11 18:40:33 +0700

    update

--- flutter_testing/lib/main.dart
+++ flutter_testing/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_testing/app.dart';
-import 'package:flutter_testing/core/config/app_config.dart';
 
 void main() async {
-  await AppConfig.init();
-
   runApp(const App());
 }
